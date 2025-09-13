119187d19ee058c07052f3090697419350910885
H4rish55 <harishdp01@gmail.com>
2025-09-13 11:20:00 -0400

    added chat store

---
+++ frontend/src/store/chatStore.js
@@ -0,0 +1,21 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+export const useChatStore = create((set) => ({
+    isChatting: false,
+    reply: null,
+    usage: null,
+    chatbot: async (input) => {
+        set({ isChatting: true })
+        try {
+            const response = await axios.post("/api/v1/chat/chatbot", input)
+            set({ reply: response.data.reply,usage: response.data.usage, isChatting: false })
+        } catch (error) {
+            toast.error(error.response.data.message || "Failed to connect to chatBot")
+            set({ isChatting: false, reply: null, usage: null })
+            console.log("Error in useChatStore:", error.message)
+            throw error
+        }
+    }
+}));
