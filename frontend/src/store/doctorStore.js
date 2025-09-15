7833114ab07d5d21bb2c3da5d24b3f255b2a6e9a
H4rish55 <harishdp01@gmail.com>
2025-09-14 21:19:00 -0400

    added doctor store and fixed a bug in doctor controller

---
+++ frontend/src/store/doctorStore.js
@@ -0,0 +1,23 @@
+import { create } from 'zustand';
+import toast from 'react-hot-toast';
+import axios from 'axios';
+
+export const useDoctorStore = create((set) => ({
+    data: null,
+    total: null,
+    limit: null,
+    page: null,
+    isDoctor: false,
+    doctorPage: async () => {
+        set({ isDoctor: true })
+        try {
+            const response = await axios.get("/api/v1/doctor/patient-records")
+            set({ data: response.data.data, total: response.data.total, limit: response.data.limit, page: response.data.page, isDoctor: false })
+        } catch (error) {
+            set({ isDoctor: false, data: null, total: null, limit: null, page: null })
+            toast.error(error.response.data.message || "Error in acessing patient records")
+            console.log("Error in Doctor store", error.message)
+            throw error
+        }
+    }
+}))
